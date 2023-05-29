8f1b6df9d1a70e128995195e80677671f05dc6b1
Aaron F Stanton <afstanton@gmail.com>
2023-05-28 23:18:22 -0400

    Initial commit

---
+++ lib/foundry_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "foundry_ruby/version"
+
+module FoundryRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/foundry_ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FoundryRuby do
+  it "has a version number" do
+    expect(FoundryRuby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
